f427c9ec575d1954ea20311dcd33a87fa273a979
Tom Brus <tombrus@gmail.com>
2022-03-02 16:36:02 +0100

    add jre and jdk

--- README.md
+++ README.md
@@ -5,7 +5,15 @@ mirrorde files can be downloaded at: ```https://modelingvaluegroup.github.io/dow
 If you are using bash you can get the urls in an associative aray with:
 ```shell
 source <(curl -s -o - 'https://modelingvaluegroup.github.io/downloads/index.sh')
-echo "download latest JDK11 at: ${downloads[jdk-11-macosx-aarch64]}"
+echo "download latest JDK11 at: ${downloads_url[jdk-11-macosx-aarch64]}"
+echo "                with md5: ${downloads_md5[jdk-11-macosx-aarch64]}"
+```
+will give:
+
+```
+source <(curl -s -o - 'https://modelingvaluegroup.github.io/downloads/index.sh')
+echo "download latest JDK11 at: ${downloads_url[jdk-11-macosx-aarch64]}"
+echo "                with md5: ${downloads_md5[jdk-11-macosx-aarch64]}"
 ```
 
 ## jdk
--- make-index.sh
+++ make-index.sh
@@ -1,15 +1,30 @@
-(
-    base="https://modelingvaluegroup.github.io/downloads"
-    echo "declare -A downloads"
+base="https://modelingvaluegroup.github.io/downloads"
+forall() {
+    local name="$1"
+    local f="$2"
+
+    echo "declare -A $name"
     for d in jdk jre; do
         (   cd $d
             for z in zulu*.zip; do
+                echo "  ...$name   -   $z" 1>&2
                 j="$(sed 's/zulu\([^.]*\)[.]\([0-9.]*\)-ca-\(...\)\([0-9.]*\)-\([a-z]*\)_\([a-z0-9]*\)[.]zip/\3/' <<<$z)"
                 v="$(sed 's/zulu\([^.]*\)[.]\([0-9.]*\)-ca-\(...\)\([0-9.]*\)-\([a-z]*\)_\([a-z0-9]*\)[.]zip/\1/' <<<$z)"
                 o="$(sed 's/zulu\([^.]*\)[.]\([0-9.]*\)-ca-\(...\)\([0-9.]*\)-\([a-z]*\)_\([a-z0-9]*\)[.]zip/\5/' <<<$z)"
                 a="$(sed 's/zulu\([^.]*\)[.]\([0-9.]*\)-ca-\(...\)\([0-9.]*\)-\([a-z]*\)_\([a-z0-9]*\)[.]zip/\6/' <<<$z)"
-                echo "downloads['$j-$v-$o-$a']='$base/$d/$z'"
+                echo "$name['$j-$v-$o-$a']='$("$f" "$d" "$z")'"
             done
         )
     done
+}
+(
+    make_url() {
+        echo "$base/$1/$2"
+    }
+    forall "downloads_url" make_url
+
+    make_md5() {
+        md5 <$2
+    }
+    forall "downloads_md5" make_md5
 ) > index.sh
